1f6d46e7ab5758a309ec9dad6ffa211513f1990f
Juan Linietsky <reduzio@gmail.com>
2019-04-19 15:54:33 -0300

    Added ability for multiple images to be imported as an atlas
    This adds support for groups in the import system, which point to a single file.
    Add property hint for saving files in file field

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -66,6 +66,7 @@
 #include "editor/import/resource_importer_obj.h"
 #include "editor/import/resource_importer_scene.h"
 #include "editor/import/resource_importer_texture.h"
+#include "editor/import/resource_importer_texture_atlas.h"
 #include "editor/import/resource_importer_wav.h"
 #include "editor/plugins/animation_blend_space_1d_editor.h"
 #include "editor/plugins/animation_blend_space_2d_editor.h"
@@ -5235,6 +5236,10 @@ EditorNode::EditorNode() {
 		Ref<ResourceImporterImage> import_image;
 		import_image.instance();
 		ResourceFormatImporter::get_singleton()->add_importer(import_image);
+
+		Ref<ResourceImporterTextureAtlas> import_texture_atlas;
+		import_texture_atlas.instance();
+		ResourceFormatImporter::get_singleton()->add_importer(import_texture_atlas);
 
 		Ref<ResourceImporterCSVTranslation> import_csv_translation;
 		import_csv_translation.instance();
