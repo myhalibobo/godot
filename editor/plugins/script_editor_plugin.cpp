9bd3879a0a661558d39041fa82af2aaa3649d26a
hbina085 <hanif.ariffin.4326@gmail.com>
2019-05-22 16:38:14 -0400

    fixed problem where the script lists gets ordered even when ordering is disabled

--- editor/plugins/script_editor_plugin.cpp
+++ editor/plugins/script_editor_plugin.cpp
@@ -1566,7 +1566,15 @@ struct _ScriptEditorItemData {
 
 	bool operator<(const _ScriptEditorItemData &id) const {
 
-		return category == id.category ? sort_key < id.sort_key : category < id.category;
+		if (category == id.category) {
+			if (sort_key == id.sort_key) {
+				return index < id.index;
+			} else {
+				return sort_key < id.sort_key;
+			}
+		} else {
+			return category < id.category;
+		}
 	}
 };
 
